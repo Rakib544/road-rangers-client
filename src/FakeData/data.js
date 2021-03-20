019bc10d4f45362e6db20bb184b48e3a120c0dc1
Rakib544 <md.rakib10122003@gmail.com>
2021-03-20 13:13:34 +0600

    new commit done

--- src/FakeData/data.js
+++ src/FakeData/data.js
@@ -5,93 +5,110 @@ export const fakeData = [
         "serviceImage": 'https://i.ibb.co/0DKXM3M/Frame.png',
         "availableService": [
             {
-                'totalPassenger' : '2',
-                'cost' : '99'
+                'totalPassenger' : '3',
+                'cost' : '130',
+                'passengerIcon': 'https://i.ibb.co/P5gGZjk/peopleicon.png'
+            },
+            {
+                'totalPassenger' : '1',
+                'cost' : '45',
+                'passengerIcon': 'https://i.ibb.co/P5gGZjk/peopleicon.png'
             },
             {
                 'totalPassenger' : '2',
-                'cost' : '99'
+                'cost' : '90',
+                'passengerIcon': 'https://i.ibb.co/P5gGZjk/peopleicon.png'
             },
             {
                 'totalPassenger' : '2',
-                'cost' : '99'
-            },
-            {
-                'totalPassenger' : '2',
-                'cost' : '99'
+                'cost' : '110',
+                'passengerIcon': 'https://i.ibb.co/P5gGZjk/peopleicon.png'
             }
         ]
     },
     {
         "serviceType": 'car',
         "serviceName": 'Car',
-        "serviceImage": 'https://i.ibb.co/0DKXM3M/Frame.png',
+        "serviceImage": 'https://i.ibb.co/V2c44xg/Frame-2.png',
         "availableService": [
             {
                 'totalPassenger' : '4',
-                'cost' : '140'
+                'cost' : '90',
+                'passengerIcon': 'https://i.ibb.co/P5gGZjk/peopleicon.png'
             },
             {
                 'totalPassenger' : '4',
-                'cost' : '140'
+                'cost' : '130',
+                'passengerIcon': 'https://i.ibb.co/P5gGZjk/peopleicon.png'
+            },
+            {
+                'totalPassenger' : '1',
+                'cost' : '140',
+                'passengerIcon': 'https://i.ibb.co/P5gGZjk/peopleicon.png'
+            },
+            {
+                'totalPassenger' : '2',
+                'cost' : '150',
+                'passengerIcon': 'https://i.ibb.co/P5gGZjk/peopleicon.png'
+            },
+            {
+                'totalPassenger' : '3',
+                'cost' : '120',
+                'passengerIcon': 'https://i.ibb.co/P5gGZjk/peopleicon.png'
             },
             {
                 'totalPassenger' : '4',
-                'cost' : '140'
-            },
-            {
-                'totalPassenger' : '4',
-                'cost' : '140'
-            },
-            {
-                'totalPassenger' : '4',
-                'cost' : '140'
-            },
-            {
-                'totalPassenger' : '4',
-                'cost' : '140'
+                'cost' : '150',
+                'passengerIcon': 'https://i.ibb.co/P5gGZjk/peopleicon.png'
             },
         ]
     },
     {
         "serviceType": 'bus',
         "serviceName": 'Bus',
-        "serviceImage": 'https://i.ibb.co/0DKXM3M/Frame.png',
+        "serviceImage": 'https://i.ibb.co/0V1q1dK/Frame-1.png',
         "availableService": [
             {
-                'totalPassenger' : '8',
-                'cost' : '250'
+                'totalPassenger' : '3',
+                'cost' : '90',
+                'passengerIcon': 'https://i.ibb.co/P5gGZjk/peopleicon.png'
             },
             {
-                'totalPassenger' : '8',
-                'cost' : '250'
+                'totalPassenger' : '5',
+                'cost' : '160',
+                'passengerIcon': 'https://i.ibb.co/P5gGZjk/peopleicon.png'
             },
             {
-                'totalPassenger' : '8',
-                'cost' : '250'
+                'totalPassenger' : '6',
+                'cost' : '220',
+                'passengerIcon': 'https://i.ibb.co/P5gGZjk/peopleicon.png'
             },
             {
-                'totalPassenger' : '8',
-                'cost' : '250'
+                'totalPassenger' : '9',
+                'cost' : '250',
+                'passengerIcon': 'https://i.ibb.co/P5gGZjk/peopleicon.png'
             },
         ]
     },
     {
         "serviceType": 'train',
         "serviceName": 'Train',
-        "serviceImage": 'https://i.ibb.co/0DKXM3M/Frame.png',
+        "serviceImage": 'https://i.ibb.co/P9NVLb6/Group.png',
         "availableService": [
             {
-                'totalPassenger' : '15',
-                'cost' : '170'
+                'totalPassenger' : '8',
+                'cost' : '120',
+                'passengerIcon': 'https://i.ibb.co/P5gGZjk/peopleicon.png'
             },
             {
                 'totalPassenger' : '15',
-                'cost' : '170'
+                'cost' : '170',
+                'passengerIcon': 'https://i.ibb.co/P5gGZjk/peopleicon.png'
             },
             {
-                'totalPassenger' : '15',
-                'cost' : '170'
+                'totalPassenger' : '12',
+                'cost' : '130',
+                'passengerIcon': 'https://i.ibb.co/P5gGZjk/peopleicon.png'
             },
         ]
     }
